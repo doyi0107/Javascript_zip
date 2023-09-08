bf593aeb8e84a62eb76d93fa901270cb8d550e60
jsmme <doyi9917@naver.com>
2023-09-08 23:52:45 +0900

    test

--- app.js
+++ app.js
@@ -16,5 +16,113 @@ function handleLinkClick(event) {
     console.dir(event);
     alert("click");
 }
+const loginInput = document.querySelector("#login-form input");
+const loginForm = document.querySelector("#login-form");
+
+function onLoinSumbit(tomato){
+    tomato.preventDefault();
+    console.log(loginInput.value);
+}
+
+loginForm.addEventListener("submit", onLoinSumbit);
+
+
+const link = document.querySelector("a");
+link.addEventListener("click", handleLinkClick);
+function handleLinkClick(event) {
+    event.preventDefault();
+    console.dir(event);
+    alert("click");
+}
+const loginInput = document.querySelector("#login-form input");
+const loginForm = document.querySelector("#login-form");
+
+function onLoinSumbit(tomato){
+    tomato.preventDefault();
+    console.log(loginInput.value);
+}
+
+loginForm.addEventListener("submit", onLoinSumbit);
+
+
+const link = document.querySelector("a");
+link.addEventListener("click", handleLinkClick);
+function handleLinkClick(event) {
+    event.preventDefault();
+    console.dir(event);
+    alert("click");
+}
+const loginInput = document.querySelector("#login-form input");
+const loginForm = document.querySelector("#login-form");
+
+function onLoinSumbit(tomato){
+    tomato.preventDefault();
+    console.log(loginInput.value);
+}
+
+loginForm.addEventListener("submit", onLoinSumbit);
+
+
+const link = document.querySelector("a");
+link.addEventListener("click", handleLinkClick);
+function handleLinkClick(event) {
+    event.preventDefault();
+    console.dir(event);
+    alert("click");
+}
+const loginInput = document.querySelector("#login-form input");
+const loginForm = document.querySelector("#login-form");
+
+function onLoinSumbit(tomato){
+    tomato.preventDefault();
+    console.log(loginInput.value);
+}
+
+loginForm.addEventListener("submit", onLoinSumbit);
+
+
+const link = document.querySelector("a");
+link.addEventListener("click", handleLinkClick);
+function handleLinkClick(event) {
+    event.preventDefault();
+    console.dir(event);
+    alert("click");
+}
+const loginInput = document.querySelector("#login-form input");
+const loginForm = document.querySelector("#login-form");
+
+function onLoinSumbit(tomato){
+    tomato.preventDefault();
+    console.log(loginInput.value);
+}
+
+loginForm.addEventListener("submit", onLoinSumbit);
+
+
+const link = document.querySelector("a");
+link.addEventListener("click", handleLinkClick);
+function handleLinkClick(event) {
+    event.preventDefault();
+    console.dir(event);
+    alert("click");
+}
+const loginInput = document.querySelector("#login-form input");
+const loginForm = document.querySelector("#login-form");
+
+function onLoinSumbit(tomato){
+    tomato.preventDefault();
+    console.log(loginInput.value);
+}
+
+loginForm.addEventListener("submit", onLoinSumbit);
+
+
+const link = document.querySelector("a");
+link.addEventListener("click", handleLinkClick);
+function handleLinkClick(event) {
+    event.preventDefault();
+    console.dir(event);
+    alert("click");
+}
 
 
