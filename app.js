c463a1a2e12e6cfe4021cdd92994c2185ca6c3ee
jsmme <doyi9917@naver.com>
2023-09-09 21:11:12 +0900

    login -ing

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
 const loginInput = document.querySelector("#login-form input");
 const loginForm = document.querySelector("#login-form");
+const greeting = document.querySelector("#greeting");
 
-function onLoinSumbit(tomato){
-    tomato.preventDefault();
-    console.log(loginInput.value);
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoinSumbit(event){
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem( USERNAME_KEY, username);
+    paintGreetings(username);
 }
 
-loginForm.addEventListener("submit", onLoinSumbit);
+function paintGreetings(username) {
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username}`; 
+}
 
 
-const link = document.querySelector("a");
-link.addEventListener("click", handleLinkClick);
-function handleLinkClick(event) {
-    event.preventDefault();
-    console.dir(event);
-    alert("click");
+const savedUsername = localStorage.getItem( USERNAME_KEY);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoinSumbit);
+} else{
+    paintGreetings(savedUsername);
 }
 
 
