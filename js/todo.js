ef40fed7073a94a39dc55648e5e9c392ec108b1d
jsmme <doyi9917@naver.com>
2023-09-14 18:51:18 +0900

    todo -ing

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,37 @@ const todoForm = document.getElementById("todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function saveTodos() {
+    localStorage.setItem("todos" , JSON.stringify(toDos));
+}
+
+function deleteTodo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+function paintTodo(newTodo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "❌"
+    button.addEventListener("click" , deleteTodo);
+    li.appendChild(span);
+    li.appendChild(button);
+    todoList.appendChild(li);  
+}
+
 function todoSubmit(event) {
     event.preventDefault();
     // newTodo에 변수값 복사해놓기  
     const newTodo = todoInput.value; 
     todoInput.value = "";
+    toDos.push(newTodo);
+    paintTodo(newTodo);
+    saveTodos();
 }
 
 todoForm.addEventListener("submit", todoSubmit)
